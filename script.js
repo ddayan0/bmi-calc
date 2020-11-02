68d5cc2968959ac2983cdd32d4bcb9e50cbfd324
Dennis D <denday198@gmail.com>
2020-11-02 16:25:18 -0500

    Made more readable

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 // dennis dayan
 function BMI() {
-  var m = document.getElementById("mass").value;
+  var w = document.getElementById("weight").value;
   var h = document.getElementById("height").value;
-  var actBMI = 703 * m / (h ** 2)
+  var actBMI = 703 * w / (h ** 2)
   if (Number.isNaN(actBMI)) {
     console.log("why must you do this")
- 		document.getElementById("result").innerHTML = ("Please input a valid number for the field(s)")
+    document.getElementById("result").innerHTML = ("Please input a valid number for the field(s)")
   }
   if (actBMI < 0.00) {
     console.log("why must you do this")
     document.getElementById("result").innerHTML = ("Please input a valid number for the field(s)")
-	}
-  if (actBMI < 18.5){
-  	 document.getElementById("result").style.color = "red"
-     document.getElementById("result").innerHTML = (actBMI.toFixed(2) + ". You are underweight!")
   }
-  if(actBMI > 18.5 && actBMI <= 24.9){
-  	document.getElementById("result").style.color = "green"
+  if (actBMI < 18.5 && actBMI > 0.00) {
+    document.getElementById("result").style.color = "red"
+    document.getElementById("result").innerHTML = (actBMI.toFixed(2) + ": You are underweight!")
+  }
+  if (actBMI > 18.5 && actBMI <= 24.9) {
+    document.getElementById("result").style.color = "green"
     document.getElementById("result").innerHTML = (actBMI.toFixed(2))
   }
-  if(actBMI > 25){
-  	document.getElementById("result").style.color = "#b5ae4e"
-    document.getElementById("result").innerHTML = (actBMI.toFixed(2) + ". You are overweight!")
+  if (actBMI > 25) {
+    document.getElementById("result").style.color = "#b5ae4e"
+    document.getElementById("result").innerHTML = (actBMI.toFixed(2) + ": You are overweight!")
   }
-  if(actBMI > 30){  	
+  if (actBMI > 30) {
     document.getElementById("result").style.color = "red"
-    document.getElementById("result").innerHTML = (actBMI.toFixed(2) + ". You are obese!")
+    document.getElementById("result").innerHTML = (actBMI.toFixed(2) + ": You are obese!")
   }
 }
