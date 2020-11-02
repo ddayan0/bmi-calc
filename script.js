f8610f242337317577ebf12f1fd522871a111be4
Dennis D <denday198@gmail.com>
2020-11-02 11:53:42 -0500

    fixed some issues with script.js

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ function BMI(){
   var actBMI = 703*m/(h**2)
   if(Number.isNaN(actBMI)){
     console.log("why must you do this")
+    actBMI = "Please input a valid number for the field(s)"     
     document.getElementById("result").innerHTML = ("Please input a valid number for the field(s)")
+    // i know these statements are redundant, it just needs them to work
+    // i dont know how this works
+    // i dont want to know how this works
+    // it just works, and im ok with that
   }
   if(actBMI < 0.00){
     console.log("why must you do this")
