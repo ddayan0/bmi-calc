0176141449bfd7e01a6f240486f7f219702aabc5
Dennis D <denday198@gmail.com>
2020-11-02 04:13:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// dennis dayan
+function BMI(){
+  var m = document.getElementById("mass").value;
+  var h = document.getElementById("height").value;
+  var actBMI = 703*m/(h**2)
+  if(Number.isNaN(actBMI)){
+    console.log("why must you do this")
+    document.getElementById("result").innerHTML = ("Please input a valid number for the field(s)")
+  }
+  if(actBMI < 0.00){
+    console.log("why must you do this")
+    document.getElementById("result").innerHTML = ("Please input a valid number for the field(s)")
+  }
+  else{
+    document.getElementById("result").innerHTML = (actBMI.toFixed(2))
+  }
+
+  
+}
+
